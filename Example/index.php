01001fb21ad8634379807af41f543875f49c8ac2
Michael Fairchild <mfairchild365@gmail.com>
2011-10-03 15:45:19 -0500

    update a variable name.

--- Example/index.php
+++ Example/index.php
@@ -5,12 +5,10 @@ if (file_exists(dirname(__FILE__) . '/config.inc.php')) {
     require dirname(__FILE__) . '/config.sample.php';
 }
 
-session_start();
-
 if (isset($_GET['model'])) {
     unset($_GET['model']);
 }
 
 $router = new RegExpRouter(array('baseURL' => Example_Controller::$url, 'classDir' => dirname(__FILE__) . "/src/Example/", "classPrefix" => "Example_"));
 
-$wub = new Example_Controller($router->route($_SERVER['REQUEST_URI'], $_GET));
+$example = new Example_Controller($router->route($_SERVER['REQUEST_URI'], $_GET));
