33128837a088045be4ebb8fa386432e5c33a7d7d
mfairchild365 <mfairchild365@gmail.com>
2011-11-14 19:09:27 -0600

    update to be complient with php coding standards.

--- src/RegExpRouter/RoutesInterface.php
+++ src/RegExpRouter/RoutesInterface.php
@@ -1,44 +1,76 @@
 <?php
 /**
- * This class is a routes interface.  All Routes class must
- * implement this interface to work properly.
+ * RegExpRouter RoutesInterface
  * 
- * @author mfairchild365
- *
+ * This file cantins the routes interface class that can be
+ * optionally used for scanning routes.
+ * 
+ * PHP Version 5
+ * 
+ * LICENSE http://www.opensource.org/licenses/mit-license.php
+ * 
+ * @category Router
+ * @package  RegExpRouter
+ * @author   Michael Fairchild <mfairchild365@gmail.com>
+ * @author   Brett Bieber <brett.bieber@gmail.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version  GIT: <git_id>
+ * @link     #
  */
 namespace RegExpRouter;
+
+
+/**
+ * RoutesInterface
+ * 
+ * This class is a routes interface.  All Routes class must
+ * implement this interface to work properly.
+ *
+ * @category Router
+ * @package  RegExpRouter
+ * @author   Michael Fairchild <mfairchild365@gmail.com>
+ * @author   Brett Bieber <brett.bieber@gmail.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link     #
+ */
 abstract class RoutesInterface
 {
     /**
      * All of the Post POST for this model.
-     * @return array
+     * 
+     * @return array an associative array of routes
      */
     abstract public static function getPostRoutes();
     
     /**
      * All of the GET Routes for this model.
-     * @return array
+     * 
+     * @return array an associative array of routes
      */
     abstract public static function getGetRoutes();
     
     /**
      * All of the DELETE Routes for this model.
-     * @return array
+     * 
+     * @return array an associative array of routes
      */
     abstract public static function getDeleteRoutes();
     
     /**
      * All of the PUT Routes for this model.
-     * @return array
+     * 
+     * @return array an associative array of routes
      */
     abstract public static function getPutRoutes();
     
     /**
      * Gathers all of the Routes for this object.
      * It then adds the called class's parent's namespace to all of the routes.
+     * The namespace is added here to make development faster, and to ensure
+     * that all routes belong to only one model.
      * Finally it returns the routs with the added namespace.
      * 
-     * @return array $routes
+     * @return array $routes an associative array of routes
      */
     public static function getRoutes()
     {
@@ -56,12 +88,13 @@ abstract class RoutesInterface
     /**
      * Adds a namespace to the routes's model class.
      * 
-     * @param string $nameSpace
-     * @param array $routes
+     * @param string $nameSpace the namespace to add to the routes
+     * @param array  $routes    the routes to be modified
      * 
-     * @return array $newRoutes
+     * @return array $newRoutes the modified routes
      */
-    protected static function addNamesapces($nameSpace, array $routes) {
+    protected static function addNamesapces($nameSpace, array $routes)
+    {
         $newRoutes = array();
         
         foreach ($routes as $regex=>$route) {
